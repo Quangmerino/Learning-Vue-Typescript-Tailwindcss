f84461e3a5bb0d24c40f0653f38f7e72a852c3a8
Newpear101204 <newpear2004@gmail.com>
2025-08-26 09:18:21 +0700

    update

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <div class ="w-[1920px] h-[593px] bg-[#252641] relative right-[0px] bottom-0">
+    <div class="w-[388px] h-[82px] absolute right-[766px] top-[74px]">
+      <div class="w-[114px] h-[83px] bg-[url(@/assets/images/logo.png)] bg-cover absolute left-[0px] right-[0px]"></div>
+      <div class ="w-[156px] h-[66px]  absolute top-[9px] right-[0px] font-[Poppins] text-[#FFFFFF] text-[22px] font-semibold">
+        Virtual Class for Zoom
+      </div>
+    </div>
+
+    <div class ="w-[440px] h-[39px] font-medium text-[26px] text-[#B2B3CF] text-center absolute top-[252px] right-[740px]">
+      Subscribe to get our Newsletter
+    </div>
+    <div class ="w-[599px] h-[60px] absolute right-[660px] top-[311px] flex justify-center gap-[20px]">
+      <div class ="h-full w-[400px] text-[#83839A] font-[Poppins] pl-[30px]  flex  items-center font-normal text-[20px] rounded-[80px] border border-[#83839A]">
+        Your Email
+      </div>
+
+      <div class ="h-full w-[179px] bg-[#49BBBD] pt-[14px] text-[22px] text-[#FFFFFF] text-center rounded-[60px]">
+        Subscribe
+      </div>
+    </div>
+
+    <div class ="w-[564px] h-[33px] absolute right-[678px] bottom-[93px] flex justify-between gap-[40px]">
+      <div class ="h-full font-[Poppins] text-[22px] font-normal text-[#B2B3CF]">Careers</div>
+      <div class ="h-full font-[Poppins] text-[22px] font-normal text-[#B2B3CF]">Privacy Policy</div>
+      <div class ="h-full font-[Poppins] text-[22px] font-normal text-[#B2B3CF]">Terms & Conditions</div>
+    </div>
+    <div class ="w-[358px] h-[33px] absolute right-[781px] bottom-[40px] font-[Poppins] text-[22px] text-[#B2B3CF] font-normal">
+      © 2021 Class Technologies Inc.
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,7 +1,104 @@
 <template>
-  <div>
-    <img src="@/assets/images/logo.png" alt="totc-logo">
+  <div class ="w-[1920px] h-[1119px] bg-[#49BBBD] p-[30px] rounded-b-[200px]  relative">
+    <div class ="bg-[#49BBBD] w-[1665px] h-[83px] flex justify-between mt-[30px] ml-[121px] mr-[126px]">
+
+      <div class ="w-[114px] h-[83px] bg-[url(@/assets/images/logo.png)] bg-cover text-center text-[32px] font-[700] text-white">
+      </div>
+      <div class ="w-[721px] h-[33px] flex justify-between items-center text-[white] text-[22px] mr-[81px] ml-[403px] mb-[31px] mt-[19px] font-[Home] font-[400px]">
+        <div>Home</div>
+        <div>Courses</div>
+        <div>Careers</div>
+        <div>Blog</div>
+        <div>About Us</div>
+      </div>
+      <div class ="w-[346px] h-[61px] flex justify-center gap-[26px]">
+        <div class ="w-[160px] h-[60px] bg-[#FFFFFF] mt-[4px] mb-[18px] rounded-[80px]">
+          <div class ="text-[22px] font-[Poppins] w-[61px] h-[33px] ml-[50px] mr-[49px] mt-[13px] mb-[14px]">Login</div>
+        </div>
+        <div class ="w-[160px] h-[60px] bg-[#FFFFFF4D] mt-[4px] mb-[18px] rounded-[80px]">
+          <div class ="text-[22px] text-[white] font-[Poppins] w-[87px] h-[33px] ml-[50px] mr-[49px] mt-[13px] mb-[14px]">Sign Up</div>
+        </div>
+      </div>
+    </div>
+
+    <div class ="w-[681px] h-[404px] mt-[222px] ml-[140px]">
+      <div class ="w-full h-[162px] text-[54px] font-[Poppins] font-bold text-[white]">
+        Studying Online is now much easier
+      </div>
+      <div class ="w-[523px] h-[76px] mt-[34px] mr-[158px] font-normal text-[24px] text-[white]">
+        TOTC is an interesting platform that will teach you in more an interactive way
+      </div>
+      <div class ="w-[604px] h-[80px] mt-[52px] mr-[71px] flex gap-[40px]">
+        <div class ="w-[220px] h-full rounded-[80px] bg-[#FFFFFF4D] text-[24px] text-[white] px-[38px] py-[22px]  font-semibold ">
+          Join for free
+
+        </div>
+
+        <div class ="w-[344px] h-[80px] flex gap-[32px] ">
+          <div class ="w-[80px] h-[80px] bg-[white] rounded-[80px]">
+            <div class ="w-[33.33px] h-[32px] bg-[#23BDEE] mx-[24px] my-[24px] rotate-90 rounded-[1px]"></div>
+
+          </div>
+          <div class ="w-[232px] h-[36px] my-[22px] text-[24px]">
+            Watch how it works
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+    </div>
+
+    <div class ="w-[544px] h-[892px] top-[224px] right-[280px] bg-[url(@/assets/images/anh1.png)] absolute bg-cover">
+    </div>
+    <div class ="w-[300px] h-[100px] absolute top-[409px] right-[717px] bg-[white] rounded-[20px] ">
+      <div class ="w-[50px] h-[50px] absolute top-[25px] left-[24px] bg-[#23BDEE] rounded-[10px] ">
+        <el-icon size="50" color="white"><Calendar /></el-icon>
+      </div>
+      <div class ="w-[58px] h-[43px] absolute top-[15px] left-[132px] text-[24px] font-[Nunito Sans] font-bold text-[#595959]">
+        250K
+      </div>
+      <div class ="w-[162px] h-[36px] absolute top-[49px] left-[110px] text-[20px] font-[Nunito Sans]  text-[#595959]">Assisted Student</div>
+
+    </div>
+
+    <div class ="w-[69px] h-[69px] absolute top-[354px] right-[218px] bg-[url(@/assets/images/bieudo.png)] bg-cover">
+
+    </div>
+
+    <div class ="w-[390px] h-[187px] absolute top-[744px] right-[639px] bg-[white] rounded-[20px]">
+      <div class="w-[56px] h-[56px] absolute top-[26px] left-[26px] bg-[url(@/assets/images/avatar.jpg)] bg-cover rounded-[56px]">
+      </div>
+      <div class="w-[20px] h-[20px] absolute top-[65px] left-[63px] bg-[#2EBB5E] bg-cover rounded-[20px]">
+      </div>
+      <div class ="w-[257px] h-[43px] absolute top-[20px] left-[105px] text-[24px] text-[#595959] font-bold font-[Nunito Sans]">
+        User Experience Class
+      </div>
+      <div class ="w-[257px] h-[43px] absolute top-[54px] left-[105px] text-[20px] text-[#595959] font-[Nunito Sans]">
+        Today at 12.00 PM
+      </div>
+      <div class ="absolute top-[109px] left-[105px] text-[20px] text-center w-[180px] h-[50px] bg-[#D8587E] rounded-[80px] text-[white] font-bold ">
+        Join now</div>
+
+
+    </div>
+
+    <div class ="w-[370px] h-[110px] absolute top-[604px] right-[118px] bg-[white] rounded-[20px]">
+      <div class ="w-[50px] h-[50px] absolute top-[30px] left-[26px] bg-[#F88C3D] rounded-[10px] ">
+        <el-icon size="50" color="white"><Message /></el-icon>
+      </div>
+      <div class ="w-[186px] h-[43px] absolute top-[20px] left-[100px] text-[24px] font-[Nunito Sans] font-bold  text-[#595959]">
+        Congratulations
+      </div>
+      <div class ="w-[249px] h-[36px] absolute top-[54px] left-[100px] text-[20px] font-[Nunito Sans]  text-[#595959]">
+        Your admission completed
+      </div>
+
+    </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
